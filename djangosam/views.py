63fce6e324379a27786612e6372bdca3ad3d4f4f
eMobilis <b3tt99@gmail.com>
2023-02-17 09:06:25 +0300

    added templates

--- djangosam/views.py
+++ djangosam/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'home.html')
-
-
 def about(request):
     return render(request, 'about.html')
 
 
-def services(request):
-    return render(request, 'services.html')
+def blog(request):
+    return render(request, 'blog.html')
 
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def services(request):
+    return render(request, 'services.html')
